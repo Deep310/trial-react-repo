2848e89cd2c7232a49d54dc9780e369ae45d2b2a
Deep310 <dparekh2019@gmail.com>
2020-06-27 15:31:43 -0400

    Updates

---
+++ precache-manifest.f08740d44418801bc76b86cfb6e6c320.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f24275d022b291f99ecd8b72cc2189f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81a0052e5c7e348fb537",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ac48db548bc552f55558",
+    "url": "/static/js/2.e44a8349.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e44a8349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a0052e5c7e348fb537",
+    "url": "/static/js/main.e6ea6400.chunk.js"
+  },
+  {
+    "revision": "dd13a9604c330aee3dcf",
+    "url": "/static/js/runtime-main.dcd9e920.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

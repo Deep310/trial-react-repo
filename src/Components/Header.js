4b69647054f5aca76c12941bcf3d95739b2fd11d
Deep310 <dparekh2019@gmail.com>
2020-06-29 21:22:10 -0400

    changed header files

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,20 +6,19 @@ import HeaderCSS from './Header.css';
 class Header extends React.Component {
     render(){ 
         return(
-            <header style = {HeaderCSS} className = 'html'>
-                <div className = "header">
- 
-                    <h1 className = "myname"> Deep Parekh <span role = "img" aria-label = "smiley-emoji">😄</span></h1>
-                    
-                    <h3 className = "about"><a href = "#about">about me</a></h3>
 
-                    <h3 className = "skills"><a href = "#skills">skills</a></h3>
-
-                    <h3 className = "contact"><a href = "#footer-text">contact me</a></h3>
-                                        
+            <header style = {HeaderCSS} className = "header"> 
+                <div class = "logo">
+                    <h4>Deep Parekh<span role = "img" aria-label = "smiley-emoji">😄</span></h4>
                 </div>
-                
+
+                <ul class = "nav-links">
+                    <li><a href = "#about">About me</a></li>
+                    <li><a href = "#skills">Skills</a></li>
+                    <li><a href = "#contact">Contact me</a></li>
+                </ul>
             </header>
+      
         )
     }
 }

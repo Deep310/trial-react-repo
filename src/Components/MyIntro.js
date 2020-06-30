44c8859ae8243bddd7f5f1ac6e8b987835237e1a
Deep310 <dparekh2019@gmail.com>
2020-06-30 17:25:04 -0400

    made header and main intro sections responsive

--- src/Components/MyIntro.js
+++ src/Components/MyIntro.js
@@ -1,48 +1,30 @@
 import React from 'react';
 import MyIntroCSS from './MyIntro.css';
-import MyPicture from './img/myImage.jpg';
+import MyPicture from './img/myImagev2.jpg';
 
 class MyIntro extends React.Component {
     render(){
         return(
-            <div style = {MyIntroCSS} className = "main-intro">
+            
+            <section style = {MyIntroCSS} className = "main-intro">
                 <div className = "text">
-                    <h1>
-                        <span className = 'move'>H</span>
-                        <span className = 'move'>i</span>
-                        <span className = 'move'>,</span>
-                        <span className = 'move'>I</span>
-                        <span className = 'move'>'</span>
-                        <span className = 'move'>m</span>
-                        <br></br>
-                        <span className = 'name'>D</span>
-                        <span className = 'name'>e</span>
-                        <span className = 'name'>e</span>
-                        <span className = 'name'>p</span>
-                        <span className = 'firstletter'>P</span>
-                        <span className = 'surname'>a</span>
-                        <span className = 'surname'>r</span>
-                        <span className = 'surname'>e</span>
-                        <span className = 'surname'>k</span>
-                        <span className = 'surname'>h</span>
-                        <span className = 'surname'>.</span>
-                    </h1>
+                    <p className = 'move hi'>Hi, I'm</p>
                     
-                    <p>Student / Programmer / Web Developer</p>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <a href="#footer-text" className="myButton">Let's get in touch!</a>
-                    
-                    
-                </div>
-
-                <div className = "image">
-                    <img src = {MyPicture} alt = "Deep Parekh"></img>
-                </div>
-
+                    <p className = 'move name'>Deep Parekh.</p>
                 
-            </div>
+                    <p className = "titles"> Student / Programmer / Web Enthusiast</p>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <a className = "btn" href = "#contact"> Let's get in touch!</a>
+                </div>
+                
+                <div className = "myimage">
+                    <img src = {MyPicture} alt = "Deep Parekh" />
+                </div>
+            </section>
+        
+
         )
     }
 }

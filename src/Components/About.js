7a586d5b12a2676037285846fbab9d8eb841e384
Deep310 <dparekh2019@gmail.com>
2020-07-30 23:36:52 -0400

    trying to increase responsiveness

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,28 +7,50 @@ import myPicture from './img/myImagev2-min.jpg';
 
 class About extends Component{
     render(){
-        return(
-            <div className="About">
-                <Container fluid>
-                    <Row>
-                        <Col xs={12} sm={12} md={12} lg={5}>
-                            <img id="myImage" alt='Deep Parekh' src={myPicture} />
-                        </Col>
-                        <Col>
-                            <p className='heading text-center font-weight-bold'>About Me</p>
-                            <p className='p1'>Hello! I'm a rising sophomore at <a id='my-rutgers' href='https://newbrunswick.rutgers.edu/'>Rutgers University
-                             - New Brunswick</a> with a major in Computer Science and minor in Business
-                              Administration and Cognitive Science. I enjoy learning new technologies
-                               and creating projects out of what I learn to really enhance the knowledge I
-                              acquired. Being a web enthusiast, I like to work on a variety of 
-                              interesting and meaningful projects that involve both, frontend and
-                               backend management. On campus, I serve as an Education Chair of Rutgers USACS, 
-                                working on bringing the CS community on campus together through educational events. </p>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>       
-        )
+        let width = window.innerWidth;
+        if(width < 256){
+            return(
+                <div className="About">
+                    <Container fluid>
+                        <Row>
+                            <Col xs={12} sm={12} md={12} lg={5}>
+                                <img id="myImage" alt='Deep Parekh' src={myPicture} />
+                            </Col>
+                            <Col>
+                                <p className='heading text-center font-weight-bold'>About Me</p>
+                                <p className='p1'>hi </p>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>       
+            )
+        }
+        else{
+            return(
+
+                <div className="About">
+                    <Container fluid>
+                        <Row>
+                            <Col xs={12} sm={12} md={12} lg={5}>
+                                <img id="myImage" alt='Deep Parekh' src={myPicture} />
+                            </Col>
+                            <Col>
+                                <p className='heading text-center font-weight-bold'>About Me</p>
+                                <p className='p1'>Hello! I'm a rising sophomore at <a id='my-rutgers' href='https://newbrunswick.rutgers.edu/'>Rutgers University
+                                 - New Brunswick</a> with a major in Computer Science and minor in Business
+                                  Administration and Cognitive Science. I enjoy learning new technologies
+                                   and creating projects out of what I learn to really enhance the knowledge I
+                                  acquired. Being a web enthusiast, I like to work on a variety of 
+                                  interesting and meaningful projects that involve both, frontend and
+                                   backend management. On campus, I serve as an Education Chair of Rutgers USACS, 
+                                    working on bringing the CS community on campus together through educational events. </p>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>       
+            )
+        }
+        
     }
 }
  

efdae188e7142e0ec1dfe5ba336f1685658a4ba2
Deep310 <dparekh2019@gmail.com>
2020-08-16 15:35:11 -0400

    edited myIntro and header sections :octocat:

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,73 +8,67 @@ import './Header.css';
 class Header extends Component {
     render(){ 
         return(
+            <Container fluid className="header">
+                <Row className="header--row">
+                    <Col xs={2} sm={3} md={5} lg={7} className="text-left">
+                        <p className="header--logo">
+                            Deep Parekh 
+                        </p>
+                    </Col>
+                    <Col className="text-right">
+                        <ul className="header--navbar">
+                            <li className="header--navbar-item">
+                                <Link
+                                    to="About"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-10}
+                                    duration={500}
+                                    >
+                                    About
+                                </Link>
+                            </li>
 
-            <div className="header">
-                <Container fluid >
-                    <Row>
-                        <Col xs={2} sm={3} md={5} lg={7} className="text-left">
-                            <p className="myName">
-                                Deep Parekh 
-                            </p>
-                        </Col>
-                        <Col className="text-right">
-                            <div className="nav-content">
-                                <ul className="nav-items">
-                                    <li className="nav-item">
-                                        <Link
-                                            to="About"
-                                            spy={true}
-                                            smooth={true}
-                                            offset={-10}
-                                            duration={500}
-                                            >
-                                            About
-                                        </Link>
-                                    </li>
-
-                                    <li className="nav-item">
-                                        <Link
-                                            activeClass="active"
-                                            to="Experience"
-                                            spy={true}
-                                            smooth={true}
-                                            offset={-70}
-                                            duration={700}
-                                        >
-                                        Experience
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link
-                                            activeClass="active"
-                                            to="Hobbies"
-                                            spy={true}
-                                            smooth={true}
-                                            offset={-20}
-                                            duration={800}
-                                        >
-                                        Hobbies
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link
-                                            activeClass="active"
-                                            to="Contact"
-                                            spy={true}
-                                            smooth={true}
-                                            offset={-20}
-                                            duration={900}
-                                        >
-                                        Contact
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-      
+                            <li className="header--navbar-item">
+                                <Link
+                                    activeClass="active"
+                                    to="Experience"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-70}
+                                    duration={700}
+                                >
+                                Experience
+                                </Link>
+                            </li>
+                            <li className="header--navbar-item">
+                                <Link
+                                    activeClass="active"
+                                    to="Hobbies"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-20}
+                                    duration={800}
+                                >
+                                Hobbies
+                                </Link>
+                            </li>
+                            <li className="header--navbar-item">
+                                <Link
+                                    activeClass="active"
+                                    to="Contact"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-20}
+                                    duration={900}
+                                >
+                                Contact
+                                </Link>
+                            </li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
--- src/Components/MyIntro.js
+++ src/Components/MyIntro.js
@@ -6,17 +6,17 @@ import { Link , animateScroll as scroll }  from 'react-scroll';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import './MyIntro.css';
+import symbol from './img/symbol.png';
 
-
+//adding custom styles to button
 const StyledButton = withStyles({
     root: {
-      background: 'black',
       borderRadius: 5,
       border: '2px solid #988345',
       color: '#988345',
       height: 50,
       padding: '0 30px',
-      marginBottom: '80px'
+      marginBottom: '6rem'
     },
     label: {
       fontSize: '1.2rem',
@@ -27,51 +27,43 @@ const StyledButton = withStyles({
 
 class MyIntro extends Component {
     render(){
-        return( 
-            
-            <section className = "Intro">
-                <Container fluid >
-                    <Row>
-                        <Col>
-                            <p className="greeting">Hi, my name is</p>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <p className="text-uppercase font-weight-bold name">Deep Parekh</p>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <div className='box'></div>
+        return(
+            <Container fluid className="intro">
+                <Row>
+                    <Col className="intro--symbol">
+                        <img alt="coding symbol" src={symbol} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p className="text-uppercase font-weight-bold intro--name">Deep Parekh</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div className='intro--line'></div>
 
-                            <p className="roles text-center">Student / Programmer / Web Enthusiast</p>
+                        <p className="intro--roles text-center">Student | Programmer | Web Enthusiast</p>
 
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <StyledButton onClick={scroll.scrollToBottom}>
-                                <Link
-                                    activeClass="active"
-                                    to="Contact"
-                                    spy={true}
-                                    smooth={true}
-                                    offset={-20}
-                                    duration={900}
-                                >
-                                    Get In Touch
-                                </Link>
-                            </StyledButton>
-                             
-                                            
-                        </Col>
-                    </Row>                    
-                </Container>
-                
-            </section>
-        
-
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <StyledButton onClick={scroll.scrollToBottom}>
+                            <Link
+                                activeClass="active"
+                                to="Contact"
+                                spy={true}
+                                smooth={true}
+                                offset={-20}
+                                duration={900}
+                            >
+                                Get In Touch
+                            </Link>
+                        </StyledButton>            
+                    </Col>
+                </Row>                    
+            </Container>       
         )
     }
 }

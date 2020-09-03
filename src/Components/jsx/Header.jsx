f5128ee66673dd08b7bfed5fb9f17083e5325976
Deep310 <dparekh2019@gmail.com>
2020-09-03 02:26:10 -0400

    trying to remove black space

--- src/Components/jsx/Header.jsx
+++ src/Components/jsx/Header.jsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Link } from 'react-scroll';
+import '../css/Header.css';
 
 class Header extends Component {
     render(){ 

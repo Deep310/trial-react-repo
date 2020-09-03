580c43fc3b03a485c9754d544a1178e681fb3d14
Deep310 <dparekh2019@gmail.com>
2020-09-03 03:11:28 -0400

    trying to remove black space

--- src/Components/jsx/Header.jsx
+++ src/Components/jsx/Header.jsx
@@ -8,66 +8,63 @@ import '../css/Header.css';
 class Header extends Component {
     render(){ 
         return(
-            <Container fluid className="header">
-                <Row className="header--row">
-                    <p className="header--logo">
-                        Deep Parekh 
-                    </p>
-                    
+            <div className="header">
+                <p className="header--logo">
+                    Deep Parekh 
+                </p>
                 
-                    <ul className="header--navbar">
-                        <li className="header--navbar-item first-item">
-                            <Link
-                                to="about"
-                                spy={true}
-                                smooth={true}
-                                offset={0}
-                                duration={500}
-                                >
-                                About
-                            </Link>
-                        </li>
+            
+                <ul className="header--navbar">
+                    <li className="header--navbar-item first-item">
+                        <Link
+                            to="about"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={500}
+                            >
+                            About
+                        </Link>
+                    </li>
 
-                        <li className="header--navbar-item">
-                            <Link
-                                activeClass="active"
-                                to="experience"
-                                spy={true}
-                                smooth={true}
-                                offset={0}
-                                duration={700}
-                            >
-                            Experience
-                            </Link>
-                        </li>
-                        <li className="header--navbar-item">
-                            <Link
-                                activeClass="active"
-                                to="Hobbies"
-                                spy={true}
-                                smooth={true}
-                                offset={-20}
-                                duration={800}
-                            >
-                            Hobbies
-                            </Link>
-                        </li>
-                        <li className="header--navbar-item">
-                            <Link
-                                activeClass="active"
-                                to="contact"
-                                spy={true}
-                                smooth={true}
-                                offset={-20}
-                                duration={900}
-                            >
-                            Contact
-                            </Link>
-                        </li>
-                    </ul>
-                    
-                </Row>
-            </Container>
+                    <li className="header--navbar-item">
+                        <Link
+                            activeClass="active"
+                            to="experience"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={700}
+                        >
+                        Experience
+                        </Link>
+                    </li>
+                    <li className="header--navbar-item">
+                        <Link
+                            activeClass="active"
+                            to="Hobbies"
+                            spy={true}
+                            smooth={true}
+                            offset={-20}
+                            duration={800}
+                        >
+                        Hobbies
+                        </Link>
+                    </li>
+                    <li className="header--navbar-item">
+                        <Link
+                            activeClass="active"
+                            to="contact"
+                            spy={true}
+                            smooth={true}
+                            offset={-20}
+                            duration={900}
+                        >
+                        Contact
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         )
     }
 }
